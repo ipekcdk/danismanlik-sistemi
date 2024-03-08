b01dd1fc6b0b1c806a5a529f7a85af09726d34a9
ipekcdk <ipekcidik@gmail.com>
2024-03-08 16:29:38 +0300

    login image

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
+    <View className='bg-red-500 w-12 h-12'>
+      <View>
+        <Image source={require('../assets/images/login.png')} />
+      </View>
+
+      <View>
+        <Text>Selam</Text>
+      </View>
+
     </View>
   )
 }
