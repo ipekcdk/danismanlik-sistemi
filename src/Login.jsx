9a8054a5556b7a79ef6a6d8dcb22993f89dceb54
ipekcdk <ipekcidik@gmail.com>
2024-03-11 11:39:27 +0300

    login screen

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,83 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView, KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
+import { Button, TextInput } from 'react-native-paper'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Login() {
   return (
-    <View className='bg-red-500 w-12 h-12'>
-      <View>
-        <Image source={require('../assets/images/login.png')} />
-      </View>
+    <KeyboardAvoidingView 
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20} // İstenirse düzenlenebilir.
+    >
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/images/login.png')} style={{ width: 200, height: 200 }}/>
+        </View>
 
-      <View>
-        <Text>Selam</Text>
-      </View>
+        <View style={styles.inputContainer}>
 
-    </View>
+          <View >
+            <Ionicons style={styles.inputIcon} name="person" size={24} color="#666" />
+            <TextInput style={styles.input} placeholder='E-mail Adresi' />
+
+          </View>
+
+          <View>
+            <Ionicons style={styles.inputIcon} name="lock-closed" size={24} color="#666" />
+            <TextInput style={styles.input} placeholder='Şifre' secureTextEntry={true} />
+          </View>
+        </View>
+
+        <Button mode="contained" style={styles.loginButton} labelStyle={{ fontWeight: 'bold', fontSize: 16 }}>
+          Giriş Yap
+        </Button>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 60,
+
+  },
+  logoContainer: {
+    marginBottom: 40,
+  },
+  inputContainer: {
+    width: '80%',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 50,
+    borderRadius: 5,
+    borderWidth: 0.5,
+    borderColor: '#ccc',
+    height: 50,
+    marginBottom: 20,
+  },
+  inputIcon: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    zIndex: 1, 
+  },  
+  loginButton: {
+    backgroundColor: '#007bff',
+    width: '80%',
+    height: 50,
+    borderRadius: 10,
+    borderColor: '#231a06',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
